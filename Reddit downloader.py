eebe674cf0e60e475b61d4fcc6685bd33758d60d
ChristofferDreist <christoffer.dreist@gmail.com>
2023-03-06 15:02:21 +0100

    new file:   Reddit downloader.py

---
+++ Reddit downloader.py
@@ -0,0 +1,4 @@
+from RedDownloader import RedDownloader
+
+RedDownloader.Download("https://www.reddit.com/r/EarthPorn/comments/7t0swm/so_glad_i_got_up_for_this_15f_sunrise_theres_only/", output = 'Test')
+
