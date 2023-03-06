13882c2689373c24d02b53eaaa1c1f302d4143eb
ChristofferDreist <christoffer.dreist@gmail.com>
2023-03-06 16:35:15 +0100

    new file:   Data/Test.jpeg 	modified:   Reddit downloader.py

--- Reddit downloader.py
+++ Reddit downloader.py
@@ -1,4 +1,16 @@
+
 from RedDownloader import RedDownloader
+import praw
 
-RedDownloader.Download("https://www.reddit.com/r/EarthPorn/comments/7t0swm/so_glad_i_got_up_for_this_15f_sunrise_theres_only/", output = 'Test')
 
+## Test for at se om det downloader
+# RedDownloader.Download("https://www.reddit.com/r/EarthPorn/comments/7t0swm/so_glad_i_got_up_for_this_15f_sunrise_theres_only/", output = 'Test', destination ="./Data")
+
+
+##Download images
+#posts = RedDownloader.DownloadBySubreddit('EarthPorn', 10)
+#authors = posts.GetPostAuthors()
+#title = posts.GetPostTitles
+
+
+# %%
